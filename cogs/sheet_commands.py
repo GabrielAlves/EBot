62fb1e65c09dda291ed7f2fee8200da15218b30b
GabrielAlves <gabriel0x3d@gmail.com>
2026-04-18 12:12:47 -0300

    [add] adição de names e aliases para os comandos, adição do comando abrir_sobre e melhora na formatação do abrir_ajuda

--- cogs/sheet_commands.py
+++ cogs/sheet_commands.py
@@ -6,7 +6,7 @@ class SheetCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(name = "ler", aliases = ["ler_valor"])
     async def ler_valor(self, ctx, nome, dia):
         try:
             valor = self.bot.sheet.ler_valor(nome, dia)
@@ -16,17 +16,18 @@ class SheetCommands(commands.Cog):
             print(e)
             await ctx.send("Erro ao enviar mensagem. tente de novo")
     
-    @commands.command()
+    @commands.command(name="escrever", aliases=["escrever_valor"])
     async def escrever_valor(self, ctx, nome, dia, valor):
         try:
-            valor = self.bot.sheet.escrever_valor(nome, dia, valor)
-            await ctx.send(valor)
+            header, tabela = self.bot.sheet.escrever_valor(nome, dia, valor)
+            tabela = self.bot.tabulacao.tabular(tabela, header)
+            await ctx.send(f'```{tabela}```')
         
         except Exception as e:
             print(e)
             await ctx.send("Erro ao enviar mensagem. tente de novo")
     
-    @commands.command()
+    @commands.command(name="tabela", aliases=["ler_tabela"])
     async def ler_tabela(self, ctx):
         try:
             header, tabela = self.bot.sheet.ler_tabela()
@@ -38,17 +39,17 @@ class SheetCommands(commands.Cog):
             await ctx.send("Erro ao enviar mensagem. tente de novo")
 
     
-    @commands.command()
-    async def limpar_tabela(self, ctx, confirmacao = None, valor = 0):
+    @commands.command(name="zerar", aliases=["zerar_tabela"])
+    async def zerar_tabela(self, ctx, confirmacao = None, valor = 0):
         try:
             if confirmacao == 'confirmar':
-                resultado = self.bot.sheet.limpar_tabela(valor)
+                resultado = self.bot.sheet.zerar_tabela(0)
 
                 if resultado:
-                    await ctx.send(f'Tabela limpa com valor {valor}')
+                    await ctx.send(f'Tabela zerada com sucesso')
 
                 else:
-                    await ctx.send(f'Erro ao tenter limpar tabela com valor {valor}')
+                    await ctx.send(f'Erro ao tenter zerar tabela')
 
             else:
                 await ctx.send(f'Passe o argumento "confirmar" para confirmar a operação')
@@ -57,18 +58,19 @@ class SheetCommands(commands.Cog):
             print(e)
             await ctx.send("Erro ao enviar mensagem. tente de novo")
     
-    @commands.command()
+    @commands.command(name="placar", aliases=["gerar_placar"])
     async def gerar_placar(self, ctx):
         try:
             header, placar = self.bot.sheet.gerar_placar()
             tabela = self.bot.tabulacao.tabular(placar, header)
             await ctx.send(f'```{tabela}```')
+            await ctx.send(f"Obs: se esse for o placar final da semana, não se esqueça de zerar a tabela com o comando '{self.bot.get_command("zerar_tabela")}'.")
         
         except Exception as e:
             print(e)
             await ctx.send("Erro ao enviar mensagem. tente de novo")
     
-    @commands.command()
+    @commands.command(name="top", aliases=["farmar_aura"])
     async def farmar_aura(self, ctx):
         try:
             header, primeiro_lugar = self.bot.sheet.farmar_aura()
@@ -79,22 +81,22 @@ class SheetCommands(commands.Cog):
             print(e)
             await ctx.send("Erro ao enviar mensgem. tente de novo")
 
-    @commands.command()
-    async def preencher_vazios(self, ctx, valor = 0):
+    @commands.command(name="completar", aliases=["completar_vazios"])
+    async def completar_zeros(self, ctx):
         try:
-            resultado = self.bot.sheet.preencher_vazios(valor)
+            resultado = self.bot.sheet.completar_zeros(0)
 
             if resultado:
-                await ctx.send(f'Vazios preenchidos com {valor}')
+                await ctx.send(f'Vazios preenchidos com 0')
 
             else:
-                await ctx.send(f'Erro ao tenter preencher com {valor}')
+                await ctx.send(f'Erro ao tenter preencher com 0')
 
         except Exception as e:
             print(e)
             await ctx.send("Erro ao enviar mensagem. tente de novo")
 
-    @commands.command()
+    @commands.command(name="link", aliases=["retornar_link"])
     async def retornar_link(self, ctx):
         try:
             link = self.bot.sheet.retornar_link()
@@ -104,72 +106,131 @@ class SheetCommands(commands.Cog):
             print(e)
             await ctx.send("Erro ao enviar mensagem. tente de novo")
 
-    @commands.command()
+    @commands.command(name="linha", aliases=["ler_linha"])
+    async def ler_linha(self, ctx, nome):
+        try:
+            header, linha = self.bot.sheet.ler_linha(nome)
+            tabela = self.bot.tabulacao.tabular(linha, header)
+            await ctx.send(f'```{tabela}```')
+
+        except Exception as e:
+            print(e)
+            await ctx.send("Erro ao enviar mensgem. tente de novo")
+
+    @commands.command(name="ajuda", aliases=["abrir_ajuda"])
     async def abrir_ajuda(self, ctx):
         embed = discord.Embed(
-            title="📖 Comandos disponíveis",
-            description="Lista de comandos do bot",
+            title="Comandos do Bot",
+            description=f"Todos os comandos devem ser precedidos por '{self.bot.user.name} '",
+            color=discord.Color.blue()
+        )
+
+        def formatar_cmd(nome, uso, descricao, exemplo, emoji):
+            cmd = self.bot.get_command(nome)
+            aliases = ", ".join(cmd.aliases) if cmd.aliases else "Nenhum"
+
+            return (
+                f"{emoji} **{cmd.name} {uso}**\n"
+                f"{descricao}\n"
+                f"**Aliases:** `{aliases}`\n"
+                f"**Exemplo:** `{self.bot.user.name} {exemplo}`"
+            )
+
+        comandos = [
+            formatar_cmd(
+                "ler", "<nome> <dia>",
+                "Lê o valor da célula correspondente a <nome> e <dia>.\n"
+                "<nome> e <dia> são case-insensitive.\n"
+                "<dia> deve ser escrito sem acentos.",
+                "ler ezequiel terca", "📖"
+            ),
+
+            formatar_cmd(
+                "escrever", "<nome> <dia> <valor>",
+                "Escreve um valor na célula correspondente.\n"
+                "<dia> deve ser escrito sem acentos.\n"
+                "<valor> deve ser um número representando minutos.",
+                "escrever fernando sabado 60", "✏️"
+            ),
+
+            formatar_cmd(
+                "tabela", "",
+                "Mostra toda a tabela formatada.\n"
+                "Se a visualização da tabela estiver bagunçada. Tente reduzir o zoom da tela pra ver melhor",
+                "tabela", "📊"
+            ),
+
+            formatar_cmd(
+                "zerar", "confirmar",
+                "Zera toda a tabela (define todos os tempos como 0).\n"
+                "Necessário usar 'confirmar' para evitar acidentes.",
+                "zerar confirmar", "⚠️"
+            ),
+
+            formatar_cmd(
+                "placar", "",
+                "Gera o ranking com base no tempo total de estudo.",
+                "placar", "🏆"
+            ),
+
+            formatar_cmd(
+                "top", "",
+                "Mostra o primeiro lugar do ranking.",
+                "top", "🥇"
+            ),
+
+            formatar_cmd(
+                "completar", "",
+                "Preenche células vazias com 0.",
+                "completar", "🧩"
+            ),
+
+            formatar_cmd(
+                "linha", "<nome>",
+                "Mostra a linha completa de um usuário.",
+                "linha carlos", "👤"
+            ),
+
+            formatar_cmd(
+                "link", "",
+                "Retorna o link da planilha.",
+                "link", "🔗"
+            ),
+
+            formatar_cmd(
+                "sobre", "",
+                "Abre o sobre (o bot).",
+                "sobre", "ℹ️"
+            ),
+        ]
+
+        for cmd in comandos:
+            embed.add_field(
+                name="\u200b",
+                value=cmd,
+                inline=False
+            )
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="sobre", aliases=["abrir_sobre"])
+    async def abrir_sobre(self, ctx):
+        embed = discord.Embed(
+            title="Sobre o Bot",
+            description=f"Este bot acessa e altera a planilha presente em {self.bot.sheet.retornar_link()}.\n"
+            f"Para ver todos os comandos disponíveis, use o comando '{self.bot.get_command("abrir_ajuda")}'\n",
             color=discord.Color.blue()
         )
 
         embed.add_field(
-            name="ler_valor <nome> <dia>",
-            value=f"Lê o valor da célula correspondente a <nome> e <dia>.\n"
-                f"<nome> e <dia> são case-insensitive, mas precisam existir na planilha.\n"
-                f"<dia> deve ser escrito sem acentos.\n"
-                f"Ex: {self.bot.user.name} ler_valor ezequiel terca",
+            name="Programador",
+            value="Gabriel Alves",
             inline=False
         )
 
         embed.add_field(
-            name="escrever_valor <nome> <dia> <valor>",
-            value=f"Escreve um valor na célula correspondente.\n<dia> deve ser escrito sem acentos.\n<valor> deve ser um número representando o tempo de estudo em minutos.\n"
-                f"Ex: {self.bot.user.name} escrever_valor fernando sabado 60",
-            inline=False
-        )
-
-        embed.add_field(
-            name="ler_tabela",
-            value=f"Mostra toda a tabela formatada.\n"
-                f"Ex: {self.bot.user.name} ler_tabela",
-            inline=False
-        )
-
-        embed.add_field(
-            name="limpar_tabela confirmar [valor]",
-            value=f"Limpa toda a tabela preenchendo com um valor (padrão = 0).\n"
-                f"É Necessário enviar 'confirmar' junto com o comando para evitar acidentes.\n"
-                f"[valor] é opcional. O default de [valor]é 0.\n"
-                f"Ex: {self.bot.user.name} limpar_tabela confirmar",
-            inline=False
-        )
-
-        embed.add_field(
-            name="gerar_placar",
-            value=f"Gera o ranking com base no tempo total de estudo.\n"
-                f"Ex: {self.bot.user.name} gerar_placar",
-            inline=False
-        )
-
-        embed.add_field(
-            name="farmar_aura",
-            value=f"Mostra o primeiro lugar do ranking.\n"
-                f"Ex: {self.bot.user.name} farmar_aura",
-            inline=False
-        )
-
-        embed.add_field(
-            name="preencher_vazios [valor]",
-            value=f"Preenche células vazias com um valor (padrão = 0).\n"
-                    f"[valor] é opcional. O default de [valor] é 0.\n"
-                f"Ex: {self.bot.user.name} preencher_vazios",
-            inline=False
-        )
-
-        embed.add_field(
-            name="retornar_link",
-            value=f"Retorna o link da planilha.\n"
-                f"Ex: {self.bot.user.name} retornar_link",
+            name="Repositório",
+            value="https://github.com/GabrielAlves/EstudoBot",
             inline=False
         )
 
